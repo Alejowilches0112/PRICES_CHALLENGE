650d9fe7186ca63029e53becfecfaa20c80a2637
cawilchesf <cawilchesf@gmail.com>
2023-01-18 16:47:17 -0500

    initializer project

---
+++ src/main/java/com/ezolucion/prices/prices_challenge/PricesChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.ezolucion.prices.prices_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricesChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezolucion/prices/prices_challenge/PricesChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezolucion.prices.prices_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
